eec41a020dead555ed47c02058bc89c5aab76958
ceign <dnssdvlp@gmail.com>
2021-04-09 21:21:17 -0400

    Work on Value card component for Estrenos section.

--- src/components/Estrenos.js
+++ src/components/Estrenos.js
@@ -1,12 +1,78 @@
 import React from "react";
 import "./Estrenos.css";
+import firstImageCard from "../images/jett.png";
 
 const Estrenos = () => {
   return (
     <section className="estrenos">
       <div className="estrenos__wrapper">
         <div className="estrenos__title">
-          <h3>Últimos estrenos</h3>
+          <h2>Últimos estrenos</h2>
+        </div>
+
+        <div className="estrenos__cards">
+          <div className="c-value__card">
+            <div className="c-value__card-image">
+              <a href="/">
+                <img src={firstImageCard} alt="" />
+              </a>
+            </div>
+            <div className="c-value__card-information">
+              <h3>
+                Valorant
+                <span>Riot Games</span>
+              </h3>
+
+              <span className="c-value__card-offer">Free to play</span>
+            </div>
+            <div className="c-value__card-price">
+              <span>$ 44.900</span>
+
+              <span>$ 30.083</span>
+            </div>
+          </div>
+
+          <div className="c-value__card">
+            <div className="c-value__card-image">
+              <a href="/">
+                <img src={firstImageCard} alt="" />
+              </a>
+            </div>
+            <div className="c-value__card-information">
+              <h3>
+                Valorant
+                <span>Riot Games</span>
+              </h3>
+
+              <span className="c-value__card-offer">Free to play</span>
+            </div>
+            <div className="c-value__card-price">
+              <span>$ 44.900</span>
+
+              <span>$ 30.083</span>
+            </div>
+          </div>
+
+          <div className="c-value__card">
+            <div className="c-value__card-image">
+              <a href="/">
+                <img src={firstImageCard} alt="" />
+              </a>
+            </div>
+            <div className="c-value__card-information">
+              <h3>
+                Valorant
+                <span>Riot Games</span>
+              </h3>
+
+              <span className="c-value__card-offer">Free to play</span>
+            </div>
+            <div className="c-value__card-price">
+              <span>$ 44.900</span>
+
+              <span>$ 30.083</span>
+            </div>
+          </div>
         </div>
       </div>
     </section>
