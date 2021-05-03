afd11fc348f7f6c3543f7c6dbd59ddd8d189f756
ceign <dnssdvlp@gmail.com>
2021-05-03 01:00:56 -0400

    Paladins Jumbotron is finished in all views.

--- src/components/PaladinsJumbotron.js
+++ src/components/PaladinsJumbotron.js
@@ -3,13 +3,17 @@ import "./PaladinsJumbotron.css";
 import PaladinsLogo from "../images/paladins-logo.webp";
 import PaladinsDemo from "../images/paladins-demo.png";
 import PaladinsCharacter_1 from "../images/paladins-character-1.webp";
+import PaladinsCharacter_2 from "../images/paladins-character-2.webp";
+import PaladinsCharacter_3 from "../images/paladins-character-3.webp";
 
 const Jumbotron = () => {
   return (
     <section className="paladins-jumbotron">
       <div className="paladins-jumbotron__wrapper">
         <div className="paladins-jumbotron__characters">
-          <img src={PaladinsCharacter_1} alt="Logo Paladins" />
+          <img src={PaladinsCharacter_1} alt="Pip" />
+          <img src={PaladinsCharacter_2} alt="Cassie" />
+          <img src={PaladinsCharacter_3} alt="Viktor" />
         </div>
 
         <div className="paladins-jumbotron__content">
